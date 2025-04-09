1184bd9c06e966f1780cacdc6eb6373a69d74bdc
Lucas Felix <lucas.fafx@gmail.com>
2025-04-09 10:32:37 -0300

    feat: validar senha criptografada com bcrypt no login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,52 +1,68 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // Registart novos usuários
 
-async function register(req, res){
-    try {
-        const {nome, email, senha } = req.body;
+async function register(req, res) {
+  try {
+    const { nome, email, senha } = req.body;
 
-        // Criptografar a senha com bcrypt
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
+    // Criptografar a senha com bcrypt
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
 
-        // Mostrar no terminal
+    // Mostrar no terminal
 
-        console.log('Dados recebidos:', { 
-            nome,
-            email,
-            senhaOriginal: senha,
-            senhaCriptografada
+    console.log("Dados recebidos:", {
+      nome,
+      email,
+      senhaOriginal: senha,
+      senhaCriptografada,
     });
 
-    res.status(201).json({ mensagem: 'Usuario registrado com sucesso!'});
-   } catch (erro) {
-    console.error('Erro ao registrar usuário:', erro);
-    res.status(500).json({ mensagem: 'Erro interno ao registrar usuario'});
-   }
+    res.status(201).json({ mensagem: "Usuario registrado com sucesso!" });
+  } catch (erro) {
+    console.error("Erro ao registrar usuário:", erro);
+    res.status(500).json({ mensagem: "Erro interno ao registrar usuario" });
+  }
 }
 
 //Login
 
-function loginUser(req, res){
-     const {email, senha} = req.body;
+async function loginUser(req, res) {
+  try {
+    const { email, senha } = req.body;
 
-    //simulando um usuário cadastrado
-
-    const  usuarioSimulado = {
-        email: 'usuario@teste.com',
-        senha: '123456'
+    // Simulando usuário que já está no banco com senha criptografada
+    const usuarioSimulado = {
+      email: "lucas@teste.com",
+      senhaCriptografada:
+        "$2b$10$Ao37b4P2.RkbiazF9D08juqQxfyfM3iZT0zf6F/rw85Hi9EPcK0Ua", // hash salvo do cadastro
     };
 
     //Verificando  se email e senha conferem
 
-    if (email === usuarioSimulado.email && senha === usuarioSimulado.senha){
-        return res.status(200).json({mensagem: 'Login Realizado com sucesso!'});
-    } else {
-        return res.status(401).json({ mensagem: 'Credenciais inválidas'});
+    if (email !== usuarioSimulado.email) {
+      return res.status(401).json({ mensagem: "Credenciais inválidas" });
     }
+
+    // COmparar senha digitada com hash salvo
+    const senhaConfere = await bcrypt.compare(
+      senha,
+      usuarioSimulado.senhaCriptografada
+    );
+
+    if (!senhaConfere) {
+      return res.status(401).json({ mensagem: "credenciais inválidas" });
+    }
+
+    // se email e senha estiverem certos
+    res.status(200).json({ mensagem: "Login realizado com sucesso!" });
+  } catch (erro) {
+    console.error("Erro no login:", erro);
+    res.status(500).json({ mensagem: "Erro interno no login" });
+  }
 }
 
 module.exports = {
-    register,
-    loginUser
+  register,
+  loginUser,
 };
