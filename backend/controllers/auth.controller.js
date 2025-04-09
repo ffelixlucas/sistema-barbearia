5e8e1da78023982498ae0736b626d56d54834e19
Lucas Felix <lucas.fafx@gmail.com>
2025-04-09 09:47:42 -0300

    feat: criar rota POST /login com validação de credenciais simuladas

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+
+// Registart novos usuários
+
 function register(req, res){
     const {nome, email, senha } = req.body;
 
@@ -6,6 +9,28 @@ function register(req, res){
     res.status(201).json({ mensagem: 'Usuario registrado com sucesso!'});
 }
 
+//Login
+
+function loginUser(req, res){
+    const {email, senha} = req.body;
+
+    //simulando um usuário cadastrado
+
+    const  usuarioSimulado = {
+        email: 'usuario@teste.com',
+        senha: '123456'
+    };
+
+    //Verificando  se email e senha conferem
+
+    if (email === usuarioSimulado.email && senha === usuarioSimulado.senha){
+        return res.status(200).json({mensagem: 'Login Realizado com sucesso!'});
+    } else {
+        return res.status(401).json({ mensagem: 'Credenciais inválidas'});
+    }
+}
+
 module.exports = {
     register,
+    loginUser
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { register } = require('../controllers/auth.controller');
+const { register, loginUser } = require('../controllers/auth.controller');
 
 router.post('/register', register);
+router.post ('/login', loginUser);
 
 module.exports = router;
